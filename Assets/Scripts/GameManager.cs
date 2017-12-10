f2969a09b279d9cbcd00d5773116442b56f35b34
Unknown <zimmermannsmicha@googlemail.com>
2017-12-10 23:46:45 +0100

    player movement

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,94 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+using System.Collections.Generic;       //Allows us to use Lists. 
+using UnityEngine.UI;                   //Allows us to use UI.
+
+public class GameManager : MonoBehaviour
+{
+    public float levelStartDelay = 2f;                      //Time to wait before starting level, in seconds.
+    public static GameManager instance = null;              //Static instance of GameManager which allows it to be accessed by any other script.
+
+    private GameObject levelImage;                          //Image to block out level as levels are being set up, background for levelText.
+    private bool doingSetup = true;                         //Boolean to check if we're setting up board, prevent Player from moving during setup.
+
+    public int level = 1;
+
+    public Scene[] ScenesToLoad;
+
+    //Awake is always called before any Start functions
+    void Awake()
+    {
+        //Check if instance already exists
+        if (instance == null)
+
+            //if not, set instance to this
+            instance = this;
+
+        //If instance already exists and it's not this:
+        else if (instance != this)
+
+            //Then destroy this. This enforces our singleton pattern, meaning there can only ever be one instance of a GameManager.
+            Destroy(gameObject);
+
+        //Sets this to not be destroyed when reloading scene
+        DontDestroyOnLoad(gameObject);
+
+        //Call the InitGame function to initialize the first level 
+        InitGame();
+    }
+
+    //this is called only once, and the paramter tell it to be called only after the scene was loaded
+    //(otherwise, our Scene Load callback would be called the very first load, and we don't want that)
+    [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.AfterSceneLoad)]
+    static public void CallbackInitialization()
+    {
+        //register the callback to be called everytime the scene is loaded
+        SceneManager.sceneLoaded += OnSceneLoaded;
+    }
+
+    //This is called each time a scene is loaded.
+    static private void OnSceneLoaded(Scene arg0, LoadSceneMode arg1)
+    {
+        instance.level++;
+        instance.InitGame();
+    }
+
+
+    //Initializes the game for each level.
+    void InitGame()
+    {
+        //While doingSetup is true the player can't move, prevent player from moving while title card is up.
+        doingSetup = true;
+
+        //SceneManager.LoadScene(ScenesToLoad[level].name);
+
+        doingSetup = false;
+
+    }
+
+
+    //Hides black image used between levels
+    void HideLevelImage()
+    {
+        //Disable the levelImage gameObject.
+        levelImage.SetActive(false);
+
+        //Set doingSetup to false allowing player to move again.
+        doingSetup = false;
+    }
+
+    //Update is called every frame.
+    void Update()
+    {
+        
+    }
+
+    //GameOver is called when the player reaches 0 food points
+    public void GameOver()
+    {
+        //Disable this GameManager.
+        enabled = false;
+    }
+}
